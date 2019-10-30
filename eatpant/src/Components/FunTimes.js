72e4cbfc483c01dd5370cd01aa949728744a2258
Michael Fearnley <44407940+FearMichael@users.noreply.github.com>
2019-10-30 18:42:37 -0400

    created react app and did demo

---
+++ eatpant/src/Components/FunTimes.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+class Fun extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            number: 0
+        };
+    }
+
+    clickyTime = () => {
+        this.setState((state) => ({
+            number: state.number + 1
+        }));
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>Hello Jeremy</h1>
+                <button onClick={this.clickyTime}>
+                    Click Here to Win!
+            </button>
+                <h1>{this.state.number}</h1>
+            </div>
+        )
+    };
+};
+// const Fun = () => {
+
+//     const [clicks, updateClicks] = useState(null);
+
+//     const decrease = () => {
+//         updateClicks(clicks - 1);
+//     }
+
+//     return (
+//         <div>
+//             <button onClick={() => updateClicks(clicks + 1)}>
+//                 Click me!
+//             </button>
+//             {(clicks &&
+//                 <h1>{clicks}</h1>
+//             ) || null}
+//             <button onClick={decrease}>
+//                 Don't Click!
+//             </button>
+//         </div>
+//     )
+// }
+
+export default Fun;
