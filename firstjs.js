89a112b66d476e19b9756dcf25d2e4f75eb1d162
Michael Fearnley <>
2021-11-01 18:13:11 -0400

    misc

--- firstjs.js
+++ firstjs.js
@@ -1,4 +1,60 @@
 
+const myString = "hello there";
+
+let myChangedString = myString + " Ralph";
+
+console.log("Ready!!");
+console.log(myString);
+console.log(myChangedString);
+
+
+myChangedString = "nothing";
+
+// console.log(myChangedString);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let myChangeableVariable;
+
+// const myNumber = 12;
+
+// const myArray = ["hi", "bob"];
+
+// const myObj = {
+//     name: "Mike",
+//     number: 12321,
+//     todos: [
+//         {
+//             completed: false,
+//             title: "learn to type"
+//         },
+//         {
+//             completed: false,
+//             title: "learn to type"
+//         }
+//     ]
+// };
+
+// console.log(myString);
+
+// myChangeableVariable = 13;
+
+// myChangeableVariable = "oopsie doopsie";
+
+
+
 // const todos = [
 //     {
 //         completed: false,
@@ -11,59 +67,46 @@
 // ]
 // todos[0]
 
-const obj = {
-    name: "Mike",
-    number: 12321,
-    todos: [
-        {
-            completed: false,
-            title: "learn to type"
-        },
-        {
-            completed: false,
-            title: "learn to type"
-        }
-    ]
-}
 
-obj.todos.forEach(function (e) {
-    console.log(e);
-})
 
-obj.todos[1]
+// obj.todos.forEach(function (e) {
+//     console.log(e);
+// })
 
-const loopMe = function (data) {
-    data.forEach(function (e) {
-        // console.log(e);
-        console.log(e.name)
-        console.log(e.phone);
-    })
-}
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(json => {
-        console.log(json);
-        loopMe(json)
-    });
+// obj.todos[1]
 
-fetch('https://jsonplaceholder.typicode.com/users/1')
-    .then(response => response.json())
-    .then(json => console.log(json));
+// const loopMe = function (data) {
+//     data.forEach(function (e) {
+//         // console.log(e);
+//         console.log(e.name)
+//         console.log(e.phone);
+//     })
+// }
+// fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(response => response.json())
+//     .then(json => {
+//         console.log(json);
+//         loopMe(json)
+//     });
 
-const heath = {
-    name: "Heath",
-    favoriteFood: "Tacos",
-    role: "Student"
-};
+// fetch('https://jsonplaceholder.typicode.com/users/1')
+//     .then(response => response.json())
+//     .then(json => console.log(json));
 
-const mike = {
-    name: "Mike",
-    favoriteFood: "Pizza",
-    role: "TA"
-};
+// const heath = {
+//     name: "Heath",
+//     favoriteFood: "Tacos",
+//     role: "Student"
+// };
 
-const myArr = [];
-const stringArr = [];
+// const mike = {
+//     name: "Mike",
+//     favoriteFood: "Pizza",
+//     role: "TA"
+// };
+
+// const myArr = [];
+// const stringArr = [];
 
 // arr = []
 // obj = {}
--- jquery/app.js
+++ jquery/app.js
@@ -1,61 +1,104 @@
-const formItem$ = $("div.form-item");
-const actionButton$ = $("button.action-button");
-const form$ = $("form.main-form");
-const input1$ = $("input.input1");
-const input2$ = $("input.input2");
-const textarea$ = $("textarea.text-area");
 
-const storageKey = "mySpecialStuff";
 
-// gets local storage .. returns the actual value.. hopefully
-const getStorage = function() {
-  return JSON.parse(localStorage.getItem(storageKey));
-}
-// Sets local storage .. no return value;
-const setStorage = function(value) {
-  localStorage.setItem(storageKey, JSON.stringify(value));
-}
+// const formItem$ = $("div.form-item"); // document.querySelector("div.form-item");
+// const actionButton$ = $("button.action-button");
+// const form$ = $("form.main-form");
+// const input1$ = $("input.input1");
+// const input2$ = $("input.input2");
+// const textarea$ = $("textarea.text-area");
+// const myContainer$ = $("div.container");
+// const storageKey = "mySpecialStuff";
 
-actionButton$.on("click", function(e) {
-  const payload = {
-    input1: input1$.val(),
-    input2: input2$.val(),
-    text1: textarea$.val(),
-    anotherItem: $(this).parent()
-  }
-  console.log($(this).parent());
 
-  setStorage(payload);
-});
+// // myContainer$.on("mousemove", function(e) {
+// //   console.log(e);
+// // })
 
-const init = function() {
-  const data = getStorage();
-  if (data) {
-    input1$.val(data.input1).css({color: "red"});
-    input2$.val(data.input2).css({color: "red"});
-    textarea$.val(data.text1).css({color: "red"});
-  }
-}
+// // gets local storage .. returns the actual value.. hopefully
+// const getStorage = function() {
+//   return JSON.parse(localStorage.getItem(storageKey));
+// }
+// // Sets local storage .. no return value;
+// const setStorage = function(value) {
+//   localStorage.setItem(storageKey, JSON.stringify(value));
+// }
 
-setTimeout(init, 3000);
+// actionButton$.on("click", function(e) {
+//   console.log(e);
+//   const payload = {
+//     input1: input1$.val(),
+//     input2: input2$.val(),
+//     text1: textarea$.val(),
+//     anotherItem: $(this).parent()
+//   }
+//   // console.log($(this).parent());
 
-// throw new Error("MY BAD!");
-// let mypotatoes;
+//   setStorage(payload);
+// });
 
-const getData = function() {
-  $.get("https://jsonplaceholder.typicode.com/todos/1").then(function(potatoes) {
-    // console.log(potatoes);
-    return Promise.resolve(potatoes);
-    console.log(mypotatoes);
-  }).catch(function(err) {
-    return Promise.reject(err);
+// const init = function() {
+//   const data = getStorage();
+//   if (data) {
+//     input1$.val(data.input1).css({color: "red"});
+//     input2$.val(data.input2).css({color: "red"});
+//     textarea$.val(data.text1).css({color: "red"});
+//   }
+// }
+
+// setTimeout(init, 3000);
+
+// // throw new Error("MY BAD!");
+// // let mypotatoes;
+
+// const getData = function () {
+//   return $.get("https://jsonplaceholder.typicode.com/todos/1").then(function (response) {
+//     console.log(response);
+//     return Promise.resolve(response);
+//   }).catch(function (err) {
+//     console.log(err);
+//     return Promise.reject(err);
+//   });
+// }
+setTimeout(() => {
+  console.log("Timeout!");
+}, 1000 * 5);
+
+console.log("Running!");
+// getData().then(stuff => console.log(stuff)).catch(err => alert(err));
+$("body").append($("<h1>").text("LOADING").addClass("loader"));
+$.get("https://jsonplaceholder.typicode.com/todos/1")
+  .then(function (response) {
+
+    console.log({ response });
+
+  })
+  .catch(function (err) {
+
     console.log(err);
+
+  })
+  .then(function () {
+    $(".loader").text("Done Loading");
+    console.log("Finally Fired")
   });
-}
-
-getData().then(stuff => console.log(stuff)).catch(err => alert(err));
-
+  
+console.log(response);
+console.log("Done Running");
 // console.log(mypotatoes);
 // $.post();
 // $.put();
 // $.delete();
+
+// const createElement = () => {
+//   const header = $("<h1>").addClass("fw-bold my-header").text("Hello World!");
+//   const div = $("<div>").addClass("text-center").append(header);
+//   $("body").append(div);
+// }
+
+// setTimeout(() => {
+//   createElement();
+// }, 3000)
+
+// setTimeout(() => {
+//   $("h1.my-header").text("Goodbye World!");
+// }, 6000);
